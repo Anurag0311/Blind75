33a0620da69b83f8a7c926bd3c0d0b1335861a4a
Anurag <anuragsingh.anurag1234@gmail.com>
2024-08-23 13:03:59 +0530

    Two Sum

---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,51 @@
+package Arrays;
+
+public class TwoSum {
+	/*
+	 * Problem Statement: Given an array of integers arr[] and an integer target.
+	 
+
+	1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+
+	2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+	Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+	Examples:
+
+	Example 1:
+	Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 14
+	Result: YES (for 1st variant)
+	       [1, 3] (for 2nd variant)
+	Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for the first variant and [1, 3] for 2nd variant.
+
+	Example 2:
+	Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 15
+	Result: NO (for 1st variant)
+		[-1, -1] (for 2nd variant)
+	Explanation: There exist no such two numbers whose sum is equal to the target.
+	
+	*/
+	
+	public String twoSum(int [] array, int target) {
+		int sum = 0;
+		for(int i = 0; i < array.length-1; i++) {
+			for (int j = 1; j < array.length; j++) {
+				if (array[i] + array[j] == target)
+					return "YES";
+			}	
+		}
+		return "NO";
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int[] arr = {2,6,5,8,11};
+		int target = 14;
+		TwoSum uos = new TwoSum();
+		System.out.println(uos.twoSum(arr, target));
+
+	}
+
+}
